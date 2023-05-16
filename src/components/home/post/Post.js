1991dfdb15f7067fc0f9b2d032b97b5f88081be0
Gangesh Jha <gangeshjha15@gmail.com>
2023-05-16 23:13:32 +0530

    Add post saved and like option in home page

--- src/components/home/post/Post.js
+++ src/components/home/post/Post.js
@@ -1,6 +1,11 @@
 import React from 'react'
-import { Box, Typography, styled } from '@mui/material'
-import { addElipsis } from '../../../utills/common-utils'; 
+import { Box, Icon, Typography, styled } from '@mui/material'
+import TurnedInNotIcon from '@mui/icons-material/TurnedInNot';
+import {
+  ThumbUp
+} from "@mui/icons-material";
+import { addElipsis } from '../../../utills/common-utils';
+import { toast } from "react-toastify";
 
 const Container = styled(Box)`
   border: 1px solid #d3cede;
@@ -26,6 +31,15 @@ const Text = styled(Typography)`
   color: #878787;
   font-size: 12px;
 `;
+const TurnedInNot = styled(TurnedInNotIcon)`
+  margin-left: 5px;
+  margin-top: 3px;
+`;
+const Thumb = styled(ThumbUp)`
+  float: right;
+  margin-right: 5px;
+  margin-top: 3px;
+`;
 const Heading = styled(Typography)`
   font-size: 18px;
   font-weight: 600;
@@ -33,6 +47,11 @@ const Heading = styled(Typography)`
 const Details = styled(Typography)`
   font-size: 14px;
   word-break: break-word;
+`;
+const LikeCount = styled(Typography)`
+  float: right;
+  margin-right: 5px;
+  margin-top: 3px;
 `;
 
 
@@ -45,6 +64,11 @@ const Post = ({post}) => {
         <Text>{post.name}</Text>
         {/* <Details noWrap>{addElipsis(post.description, 60)}</Details> */}
         <Details noWrap>{post.description}</Details>
+        <Box>
+          <TurnedInNot onClick={()=>toast.success("Post Saved Successfully!")}/>
+          <LikeCount>{post.likes ? post.likes.length : ""} likes</LikeCount>
+          <Thumb/>
+        </Box>
     </Container>
   )
 }
